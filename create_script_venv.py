5845a6cacf692843c41f08b621693061290cf06d
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2022-01-13 10:58:21 -0500

    Changed the folder structure

--- create_script_venv.py
+++ create_script_venv.py
@@ -8,6 +8,8 @@ from pathlib import Path
 from types import SimpleNamespace
 from typing import Final
 
+PROJECT_NAME: Final[str] = 'inf6200-h2022-benoit-dubreuil'
+
 ROOT_DIR: Final[Path] = Path().absolute()
 ROOT_DIR.resolve(strict=True)
 
@@ -15,20 +17,16 @@ CONF_DIR_NAME: Final[str] = 'conf'
 CONF_DIR: Final[Path] = ROOT_DIR / CONF_DIR_NAME
 CONF_DIR.resolve(strict=True)
 
-SCRIPT_DIR_NAME: Final[str] = 'script'
-SCRIPT_DIR: Final[Path] = CONF_DIR / SCRIPT_DIR_NAME
-SCRIPT_DIR.resolve(strict=True)
-
-SRC_DIR_NAME: Final[str] = 'src'
-SRC_DIR: Final[Path] = SCRIPT_DIR / SRC_DIR_NAME
-SRC_DIR.resolve(strict=True)
+SCRIPTS_DIR_NAME: Final[str] = 'scripts'
+SCRIPTS_DIR: Final[Path] = CONF_DIR / SCRIPTS_DIR_NAME
+SCRIPTS_DIR.resolve(strict=True)
 
 REQS_FILE_NAME: Final[str] = 'requirements.txt'
-REQS_FILE: Final[Path] = SCRIPT_DIR / REQS_FILE_NAME
+REQS_FILE: Final[Path] = ROOT_DIR / REQS_FILE_NAME
 REQS_FILE.resolve(strict=True)
 
 VENV_DIR_NAME: Final[str] = 'venv'
-VENV_DIR: Final[Path] = SCRIPT_DIR / VENV_DIR_NAME
+VENV_DIR: Final[Path] = CONF_DIR / VENV_DIR_NAME
 
 
 class EnvBuilderInstallReqs(venv.EnvBuilder):
@@ -45,25 +43,25 @@ class EnvBuilderInstallReqs(venv.EnvBuilder):
         self.__install_reqs(context=context)
 
     def post_setup(self, context: SimpleNamespace) -> None:
-        self.__create_src_path_config_file()
+        self.__create_scripts_path_config_file()
 
     @staticmethod
-    def __create_src_path_config_file() -> None:
+    def __create_scripts_path_config_file() -> None:
         path_config_file_dotless_extension: Final[str] = 'pth'
-        src_path_config_file_name: Final[str] = '.'.join([CONF_DIR_NAME, SCRIPT_DIR_NAME, SRC_DIR_NAME, path_config_file_dotless_extension])
+        scripts_path_config_file_name: Final[str] = '.'.join([SCRIPTS_DIR_NAME, path_config_file_dotless_extension])
         file_mode: Final[int] = 0o770
         encoding: Final[str] = encodings.utf_8.getregentry().name
 
-        print(f'Create src path config file')
+        print(f'Create scripts path config file')
         print('----------------------------')
         print('VEnv dir:', VENV_DIR)
 
-        src_path_config_file: Path = VENV_DIR / src_path_config_file_name
-        src_path_config_file.touch(mode=file_mode, exist_ok=True)
-        print('Src path config file:', src_path_config_file)
+        scripts_path_config_file: Path = VENV_DIR / scripts_path_config_file_name
+        scripts_path_config_file.touch(mode=file_mode, exist_ok=True)
+        print('Scripts path config file:', scripts_path_config_file)
 
-        src_path_config: str = os.path.relpath(path=SRC_DIR, start=VENV_DIR)
-        src_path_config_file.write_text(data=src_path_config, encoding=encoding)
+        scripts_path_config: str = os.path.relpath(path=SCRIPTS_DIR, start=VENV_DIR)
+        scripts_path_config_file.write_text(data=scripts_path_config, encoding=encoding)
 
         print()
 
@@ -96,9 +94,7 @@ class EnvBuilderInstallReqs(venv.EnvBuilder):
         return pip_cmd_args
 
 
-venv_builder = EnvBuilderInstallReqs(system_site_packages=False,
-                                     clear=True,
-                                     with_pip=True,
+venv_builder = EnvBuilderInstallReqs(with_pip=True,
                                      upgrade_deps=True)
 
 venv_builder.create(env_dir=VENV_DIR)
