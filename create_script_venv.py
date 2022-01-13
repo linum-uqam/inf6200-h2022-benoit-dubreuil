3832535884834604fc4c2c8e0f8f44dd0b938c3d
Benoît Dubreuil <benoit-dubreuil@users.noreply.github.com>
2022-01-13 11:33:29 -0500

    Added back `clear=True` option

--- create_script_venv.py
+++ create_script_venv.py
@@ -97,7 +97,8 @@ class EnvBuilderInstallReqs(venv.EnvBuilder):
         return pip_cmd_args
 
 
-venv_builder = EnvBuilderInstallReqs(with_pip=True,
+venv_builder = EnvBuilderInstallReqs(clear=True,
+                                     with_pip=True,
                                      upgrade_deps=True)
 
 venv_builder.create(env_dir=VENV_DIR)
